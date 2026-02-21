03ae7a81d0bd9fbdad0e14e08773426bf8b91ba1
Boris Ivanov <bors2908@gmail.com>
2026-02-21 17:59:43 +0300

    Add boilerplate

--- backend/common/src/main/kotlin/ru/itmo/enterprise/common/service/BaseSimpleService.kt
+++ backend/common/src/main/kotlin/ru/itmo/enterprise/common/service/BaseSimpleService.kt
@@ -7,7 +7,7 @@ import ru.itmo.enterprise.common.dto.DeleteDTO
 import ru.itmo.enterprise.common.entity.BaseEntity
 
 abstract class BaseSimpleService<E : BaseEntity, D : BaseDTO, C : BaseCreateDTO, U : BaseUpdateDTO> :
-    BaseServiceWithEvents<E, D, C, U, DeleteDTO>() {
+    BaseService<E, D, C, U, DeleteDTO>() {
     override fun getDeleteDTO(): DeleteDTO {
         return DeleteDTO(false)
     }
