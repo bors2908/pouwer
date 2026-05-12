16307f63a2ce771034bbe1b03c30f8993079e923
Boris Ivanov <bors2908@gmail.com>
2026-05-12 15:51:06 +0300

    WIP

--- backend/core-api/src/main/kotlin/ru/itmo/enterprise/challenge/api/Metadata.kt
+++ backend/core-api/src/main/kotlin/ru/itmo/enterprise/challenge/api/Metadata.kt
@@ -1,5 +1,7 @@
 package ru.itmo.enterprise.challenge.api
 
+import java.util.Objects
+
 data class NonceRange(
     val start: Long,
     val end: Long
@@ -10,17 +12,18 @@ data class ChallengePayload(
     val expiresAt: Long
 ) {
     override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        other as ChallengePayload
-        if (!random.contentEquals(other.random)) return false
-        if (expiresAt != other.expiresAt) return false
-        return true
+        if (this === other) {
+            return true
+        }
+        if (other !is ChallengePayload) {
+            return false
+        }
+
+        return Objects.equals(expiresAt, other.expiresAt)
+                && random.contentEquals(other.random)
     }
 
     override fun hashCode(): Int {
-        var result = random.contentHashCode()
-        result = 31 * result + expiresAt.hashCode()
-        return result
+        return expiresAt.hashCode() + random.contentHashCode()
     }
 }
