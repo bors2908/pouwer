a83403c361f09242adc0abde8f9fb476bc8ff1f4
Boris Ivanov <bors2908@gmail.com>
2026-05-18 00:32:39 +0300

    Bump ver

--- pouwer-core/server/core-runtime/build.gradle.kts
+++ pouwer-core/server/core-runtime/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation(platform(libs.spring.boot.bom))
     implementation(platform(libs.spring.cloud.bom))
     implementation(libs.spring.cloud.starter.openfeign)
+    implementation(libs.json)
     implementation(libs.pf4j)
 
     testRuntimeOnly(libs.junit.platform.launcher)
