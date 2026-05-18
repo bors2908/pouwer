5be5aed925c0c5eeaf319bb5f399814bfb8bb01f
Boris Ivanov <bors2908@gmail.com>
2026-05-19 01:14:27 +0300

    Add COOP and COEP to static

--- pouwer-core/server/core-runtime/src/main/kotlin/ge/becrin/pouwer/pow/controller/StaticPluginProxyController.kt
+++ pouwer-core/server/core-runtime/src/main/kotlin/ge/becrin/pouwer/pow/controller/StaticPluginProxyController.kt
@@ -2,6 +2,7 @@ package ge.becrin.pouwer.pow.controller
 
 import ge.becrin.pouwer.pow.service.StaticPluginProxyService
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
@@ -19,9 +20,21 @@ class StaticPluginProxyController(
         request: HttpServletRequest
     ): ResponseEntity<StreamingResponseBody> {
         val (statusCode, headers, body) = proxyService.proxy(pluginId, request)
+        val responseHeaders = HttpHeaders().apply {
+            putAll(headers)
+            add(COOP_HEADER, COOP_VALUE)
+            add(COEP_HEADER, COEP_VALUE)
+        }
         return ResponseEntity
             .status(HttpStatusCode.valueOf(statusCode))
-            .headers(headers)
+            .headers(responseHeaders)
             .body(body)
+    }
+
+    private companion object {
+        private const val COOP_HEADER = "Cross-Origin-Opener-Policy"
+        private const val COEP_HEADER = "Cross-Origin-Embedder-Policy"
+        private const val COOP_VALUE = "same-origin"
+        private const val COEP_VALUE = "require-corp"
     }
 }
