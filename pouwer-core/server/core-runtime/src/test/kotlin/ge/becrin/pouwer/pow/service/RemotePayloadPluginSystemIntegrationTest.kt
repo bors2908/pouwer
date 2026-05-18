6247d347da5a1cb45b08e93ce564d7e097e40a87
Boris Ivanov <bors2908@gmail.com>
2026-05-18 22:18:11 +0300

    Bump ver

--- pouwer-core/server/core-runtime/src/test/kotlin/ge/becrin/pouwer/pow/service/RemotePayloadPluginSystemIntegrationTest.kt
+++ pouwer-core/server/core-runtime/src/test/kotlin/ge/becrin/pouwer/pow/service/RemotePayloadPluginSystemIntegrationTest.kt
@@ -212,7 +212,7 @@ class RemotePayloadPluginSystemIntegrationTest {
         return PluginMetadata(
             id = id,
             version = "1.0.0",
-            contractVersion = "0.1.8",
+            contractVersion = "0.1.9",
             baseUrl = baseUrl,
             lastHeartbeat = Instant.now(),
             status = PluginStatus.HEALTHY,
