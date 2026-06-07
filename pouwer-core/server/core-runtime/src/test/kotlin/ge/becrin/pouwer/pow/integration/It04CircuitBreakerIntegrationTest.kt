8ec4dc614fc71ddc1dd1556dd08f93c53ab87165
Boris Ivanov <bors2908@gmail.com>
2026-06-07 23:40:34 +0300

    Bump ver to 0.2.2

--- pouwer-core/server/core-runtime/src/test/kotlin/ge/becrin/pouwer/pow/integration/It04CircuitBreakerIntegrationTest.kt
+++ pouwer-core/server/core-runtime/src/test/kotlin/ge/becrin/pouwer/pow/integration/It04CircuitBreakerIntegrationTest.kt
@@ -135,7 +135,7 @@ class It04CircuitBreakerIntegrationTest : IntegrationTestBase() {
             mapOf(
                 "id" to pluginId,
                 "version" to "1.0.0",
-                "contractVersion" to "0.2.1",
+                "contractVersion" to "0.2.2",
                 "baseUrl" to baseUrl
             )
         )
